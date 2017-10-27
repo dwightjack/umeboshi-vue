c538e87ebf3236ab279afe41fb8be5d3c8b9eca1
Marco Solazzi <marco.solazzi@gmail.com>
2017-10-28 00:52:14 +0200

    feat(tests): test utils moved to uemboshi-config-vue

--- template/app/assets/js/containers/Root/index.test.js
+++ template/app/assets/js/containers/Root/index.test.js
@@ -1,21 +1,21 @@
-import { mount } from '../../shared/test-utils';
+import { shallow } from 'avoriaz';
 import Root from './index';
 
 /* eslint-env jest */
 describe('<Root>', () => {
 
-    let vm;
+    let wrapper;
 
     beforeEach(() => {
-        vm = mount(Root);
+        wrapper = shallow(Root);
     });
 
     it('should render a <div> tag', () => {
-        expect(vm.$el.tagName).toBe('DIV');
+        expect(wrapper.is('div')).toBe(true);
     });
 
     it('should contain "Hello World"', () => {
-        expect(vm.$el.textContent.trim()).toBe('Hello World');
+        expect(wrapper.text().trim()).toBe('Hello World');
     });
 
 });
